dd82b9f5a64da54661f468c1dd12b95bd9bc8b1b
Rilind <rilindp@gmail.com>
2019-06-18 20:22:58 +0200

    new updates

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,6 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-5">
-				<img src="/img/Group 23.png">
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
 			</div>
 			<div class="col-md-3">
